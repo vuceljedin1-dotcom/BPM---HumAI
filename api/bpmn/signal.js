e4d7afaa9b92044bc7f3b9871012ca27c65587aa
vuceljedin1-dotcom <vucelj.edin1@gmail.com>
2025-09-24 22:35:08 +0200

    Delete api directory

--- api/bpmn/signal.js
+++
@@ -1,8 +0,0 @@
-import { callHS } from '../_util.js';
-export default async function handler(req,res){
-  if(req.method!=='POST') return res.status(405).json({error:'Method not allowed'});
-  const { instanceId, userId, summary } = req.body||{};
-  const data = await callHS({ intent:'SIGNAL', signal:'DAILY_SUMMARY', instanceId, correlationKey:userId, payload:summary||null });
-  return res.status(200).json(data);
-}
-
--- api/forms-url.js
+++
@@ -1,3 +0,0 @@
-export default async function handler(req,res){
-  res.status(200).json({ url: process.env.GOOGLE_FORM_URL || 'https://forms.gle/KkTYVmAayn6rMnoHA' });
-}
