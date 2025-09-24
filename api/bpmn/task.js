5cf518877af5deb2e7d6a517cea15d14e95f3360
vuceljedin1-dotcom <vucelj.edin1@gmail.com>
2025-09-24 22:34:38 +0200

    Update task.js

--- api/bpmn/task.js
+++ api/bpmn/task.js
@@ -1,8 +1,1 @@
-import { callHS } from '../_util.js';
-export default async function handler(req,res){
-  if(req.method!=='POST') return res.status(405).json({error:'Method not allowed'});
-  const { instanceId, userId, taskKey, result } = req.body||{};
-  const data = await callHS({ intent:'COMPLETE_USER_TASK', instanceId, correlationKey:userId, taskKey, result });
-  return res.status(200).json(data);
-}
 
