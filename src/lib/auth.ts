194f79566e9d2f4422b02167ea38239b33d7196b
vuceljedin1-dotcom <vucelj.edin1@gmail.com>
2025-09-25 14:25:14 +0200

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,1 +1,16 @@
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export function getSupabaseServer() {
+  const cookieStore = cookies();
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (key) => cookieStore.get(key)?.value,
+      },
+    }
+  );
+}
 
