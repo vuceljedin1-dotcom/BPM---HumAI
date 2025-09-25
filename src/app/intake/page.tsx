7549ca409da019ac70c645d30ede91c5ad644d62
vuceljedin1-dotcom <vucelj.edin1@gmail.com>
2025-09-25 18:15:08 +0200

    Create page.tsx

---
+++ src/app/intake/page.tsx
@@ -0,0 +1,64 @@
+Skip to content
+Edin Vucelj's projects
+Edin Vucelj's projects
+
+Hobby
+
+hum-ai
+
+
+Find…
+F
+
+Project Settings
+Go to Team Settings
+Search...
+General
+Build and Deployment
+Domains
+Promotion Requirements
+Environments
+Environment Variables
+Git
+Integrations
+Deployment Protection
+Functions
+Caches
+Cron Jobs
+Microfrontends
+Project Members
+Drains
+Security
+Secure Compute
+Advanced
+Domains
+Domains can be assigned to git branches, custom environments, and production.
+
+Search…
+
+bpm.ba
+Valid Configuration
+307
+www.bpm.ba
+View DNS Records & More for bpm.ba →
+
+
+www.bpm.ba
+Valid Configuration
+Production
+View DNS Records & More for bpm.ba →
+
+Home
+Docs
+Guides
+Help
+Contact
+Loading status…
+
+Select a display theme:
+
+system
+
+light
+
+dark
