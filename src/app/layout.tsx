bbbdf7f51f52a48a7b7e5047883b77756bff4095
vuceljedin1-dotcom <vucelj.edin1@gmail.com>
2025-09-26 16:02:36 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,17 @@
+// src/app/layout.tsx
+export const metadata = {
+  title: "BPM",
+  description: "BPM Human-Centred AI",
+};
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ fontFamily: "system-ui", margin: 0 }}>{children}</body>
+    </html>
+  );
+}
